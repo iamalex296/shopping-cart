533ea56e19b836f8fb6a19d447ddc5c7f98d62a3
alex <alexizana10@gmail.com>
2020-11-07 04:22:53 +0400

    added Cart.js component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,35 @@
+ 
 import React from "react"
 
-const Filter = () => {
-return (
-<div>
-
-</div>
-)
+const Filter = (props) => {
+  return (
+    <div className="filter">
+      <div className="filter-result">
+        {props.count} Products
+      </div>
+      <div className="filter-sort">
+        Order{" "}
+        <select value={props.sort} onChange={props.sortProducts}>  
+          <option>Latest</option>
+          <option value="lowest">Lowest</option>
+          <option value="highest">Highest</option>
+        </select>
+      </div>
+      <div className="filter-size">
+        Filter{" "}
+        <select value={props.size} onChange={props.filterProducts}>
+          <option value="ALL">ALL</option>
+          <option value="XS">XS</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+          <option value="XXL">XXL</option>
+          
+        </select>
+      </div>
+    </div>
+  )
 }
 
 export default Filter;
